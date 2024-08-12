d91341a9daa41920a3907b15989ee3843f0a307d
6353079417 <dhavalchauhan00321@gmail.com>
2024-08-12 18:24:59 +0530

    some changing

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -21,9 +21,12 @@ export class News extends Component {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
   async UpdateNews(PageNo){
+    this.props.setProgress(10);
     let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&apiKey=a89cc90426c0465a8532a27c053371c2&category=${this.props.category}&pageSize=${this.props.pageSize}&page=${PageNo}`;
     this.setState({loading:true});
+    this.props.setProgress(30);
     let data = await fetch(url);
+    this.props.setProgress(70);
     let parsedData = await data .json();
     console.log(parsedData);
     this.setState({
@@ -31,6 +34,7 @@ export class News extends Component {
       totalResults:parsedData.totalResults,
       loading:false
     });
+      this.props.setProgress(100);
     document.title=`${this.capitalizeFirstLetter(this.props.category)}- Daily Spark`;
   }
 
@@ -47,10 +51,11 @@ export class News extends Component {
       let parsedData = await data .json();
       console.log(parsedData);
       this.setState({
-        articles : parsedData.articles,
+        articles : parsedData.articles.concat(this.state.articles),
         totalResults:parsedData.totalResults,
         loading:false
       });
+
   };
   
   
@@ -66,6 +71,11 @@ export class News extends Component {
           next={this.fetchMoreData}
           hasMore={this.state.articles.length < this.state.totalResults}
           loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: 'center' }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
         >
           <div className="container">
           <div className="row ">
